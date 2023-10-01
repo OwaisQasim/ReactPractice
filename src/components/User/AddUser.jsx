15605c5149e2ec0ab6e19fdc39b9eb13c4d3a1b6
iamrfowaisqasim <rfowaisqasim@gmail.com>
2023-10-01 21:07:19 +0500

    added an error state

--- src/components/User/AddUser.jsx
+++ src/components/User/AddUser.jsx
@@ -2,20 +2,22 @@ import React, { useState } from 'react'
 import Card from '../UI/Card'
 import classes from './AddUser.module.css'
 import Button from '../UI/Button'
+import ErrorModal from '../UI/ErrorModal'
 
 export default function AddUser(props) {
 
     const [username, setUsername] = useState('')
     const [age, setAge] = useState('')
-    const [gender, setGender] = useState('')
+    const [gender, setGender] = useState('male')
+    const [error, setError] = useState()
 
     const submitHandler = (evnt) => {
         evnt.preventDefault()
         if (username.trim().length === 0 || age.trim().length === 0) {
-            return
+            setError({ title: 'Invalid Username', message: 'Please Enter a valid username.' })
         }
         if (+age < 1) {
-            return
+            setError({ title: 'Invalid Age', message: 'Please enter a valid age.' })
         }
 
         if (gender !== 'male' && gender !== 'female') {
@@ -40,18 +42,22 @@ export default function AddUser(props) {
     }
 
     return (
-        <Card className={classes.input}>
-            <form onSubmit={submitHandler}>
-                <label htmlFor="username">Username: </label>
-                <input value={username} onChange={usernameChangeHandler} id='username' type="text" />
-                <label htmlFor="age">Age (years): </label>
-                <input value={age} type="number" id="age" onChange={ageChangeHandler} />
-                <select value={gender} id='gender' onChange={genderChangeHandler}>
-                    <option value="male">Male</option>
-                    <option value="female">Female</option>
-                </select>
-                <Button type='submit'>Add User</Button>
-            </form>
-        </Card>
+        <div>
+            {error && <ErrorModal title='An Error occured!' message='Something went wrong.' />}
+
+            <Card className={classes.input}>
+                <form onSubmit={submitHandler}>
+                    <label htmlFor="username">Username: </label>
+                    <input value={username} onChange={usernameChangeHandler} id='username' type="text" />
+                    <label htmlFor="age">Age (years): </label>
+                    <input value={age} type="number" id="age" onChange={ageChangeHandler} />
+                    <select value={gender} id='gender' onChange={genderChangeHandler}>
+                        <option value="male">Male</option>
+                        <option value="female">Female</option>
+                    </select>
+                    <Button type='submit'>Add User</Button>
+                </form>
+            </Card>
+        </div>
     )
 }
